6c5c3a380b2039c8e31ee40ded71b55c43eb393f
Yu Cang <yhcy1993@gmail.com>
2019-09-09 14:57:50 +0800

    add face num calc

--- inc/plot3d.h
+++ inc/plot3d.h
@@ -6,36 +6,36 @@
 class PLOT3D_BLK
 {
 private:
-    size_t m_nI, m_nJ, m_nK;
+	size_t m_nI, m_nJ, m_nK;
 	bool m_3d;
 	int m_dim;
 	double *m_coord;
 	size_t m_nIJ, m_nIJK;
 
 public:
-	PLOT3D_BLK(size_t nI, size_t nJ):
+	PLOT3D_BLK(size_t nI, size_t nJ) :
 		m_nI(nI),
 		m_nJ(nJ),
 		m_nK(1),
 		m_3d(false),
 		m_dim(2),
-		m_coord(new double[2*nI*nJ])
+		m_coord(new double[2 * nI*nJ])
 	{
-		if(nI==0 || nJ == 0)
+		if (nI == 0 || nJ == 0)
 			throw("Invalid size.");
 
 		m_nIJK = m_nIJ = m_nI * m_nJ;
 	}
-	
-	PLOT3D_BLK(size_t nI, size_t nJ, size_t nK):
+
+	PLOT3D_BLK(size_t nI, size_t nJ, size_t nK) :
 		m_nI(nI),
 		m_nJ(nJ),
 		m_nK(nK),
 		m_3d(true),
 		m_dim(3),
-		m_coord(new double[3*nI*nJ*nK])
+		m_coord(new double[3 * nI*nJ*nK])
 	{
-		if(nI < 2 || nJ < 2 || nK < 2)
+		if (nI < 2 || nJ < 2 || nK < 2)
 			throw("Invalid size.");
 
 		m_nIJ = m_nI * m_nJ;
@@ -44,9 +44,9 @@ public:
 
 	~PLOT3D_BLK()
 	{
-		delete [] m_coord;
+		delete[] m_coord;
 	}
-	
+
 	bool is3D() const
 	{
 		return m_3d;
@@ -66,35 +66,57 @@ public:
 	{
 		size_t ret = 0;
 
-		if(is3D)
-			ret = (m_nI-1)*(m_nJ-1)*(m_nK-1);
+		if (is3D)
+			ret = (m_nI - 1)*(m_nJ - 1)*(m_nK - 1);
 		else
-			ret = (m_nI-1)*(m_nJ-1);
+			ret = (m_nI - 1)*(m_nJ - 1);
 
 		return ret;
 	}
 
 	size_t face_num() const
 	{
-		return boundary_face_num() + internal_face_num();
+		size_t ret = 0;
+
+		if (is3D)
+		{
+			ret += (m_nI - 1) * (m_nJ - 1) * m_nK;
+			ret += (m_nJ - 1) * (m_nK - 1) * m_nI;
+			ret += (m_nK - 1) * (m_nI - 1) * m_nJ;
+		}
+		else
+		{
+			ret += (m_nI - 1) * m_nJ;
+			ret += (m_nJ - 1) * m_nI;
+		}
+
+		return ret;
 	}
 
 	size_t boundary_face_num() const
 	{
 		size_t ret = 0;
 
-		// TODO
+		if (is3D)
+		{
+			ret += (m_nI - 1) * (m_nJ - 1);
+			ret += (m_nJ - 1) * (m_nK - 1);
+			ret += (m_nK - 1) * (m_nI - 1);
+			ret *= 2;
+		}
+		else
+		{
+			ret += (m_nI - 1);
+			ret += (m_nJ - 1);
+			ret *= 2;
+		}
 
 		return ret;
 	}
 
 	size_t internal_face_num() const
 	{
-		size_t ret = 0;
-
-		// TODO
-
-		return ret;
+		return face_num() - boundary_face_num();
 	}
 
 	size_t IMAX() const
@@ -106,43 +128,43 @@ public:
 	{
 		return m_nJ;
 	}
-	
+
 	size_t KMAX() const
 	{
 		return m_nK;
 	}
 
 	// 0-based
-	double *at(size_t i, size_t j) 
+	double *at(size_t i, size_t j)
 	{
 		return m_coord + STX(i, j);
 	}
 
-	double *at(size_t i, size_t j, size_t k) 
+	double *at(size_t i, size_t j, size_t k)
 	{
 		return m_coord + STX(i, j, k);
 	}
 
 	// 1-based
-	double *operator()(size_t i, size_t j) 
+	double *operator()(size_t i, size_t j)
 	{
-		return at(i-1, j-1);
+		return at(i - 1, j - 1);
 	}
 
-	double *operator()(size_t i, size_t j, size_t k) 
+	double *operator()(size_t i, size_t j, size_t k)
 	{
-		return at(i-1, j-1, k-1);
+		return at(i - 1, j - 1, k - 1);
 	}
 
 private:
 	size_t STX(size_t i, size_t j) const
 	{
-		return i + j * m_nI;
+		return 2 * (i + j * m_nI);
 	}
 
 	size_t STX(size_t i, size_t j, size_t k) const
 	{
-		return i + j * m_nI + k * m_nIJ;
+		return 3 * (i + j * m_nI + k * m_nIJ);
 	}
 };
 
@@ -150,13 +172,13 @@ private:
 class PLOT3D
 {
 private:
-    size_t m_nBLK;
-    bool m_3d;
+	size_t m_nBLK;
+	bool m_3d;
 	int m_dim;
 	std::vector<PLOT3D_BLK*> m_blk;
 
 public:
-    PLOT3D() {}
+	PLOT3D() {}
 
 	~PLOT3D() {}
 

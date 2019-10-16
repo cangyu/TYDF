ebd4009222d403692f1d20c034321c4a005f706f
仓宇 <yhcy1993@gmail.com>
2019-10-16 23:55:51 +0800

    fix cc removal

--- test/4/main.cc
+++ test/4/main.cc
@@ -3,8 +3,6 @@
 
 int test0()
 {
-	std::cout << "test0" << std::endl;
-
 	NMF::MAPPING mapping;
 	mapping.readFromFile("mapping0.nmf");
 	mapping.writeToFile("mapping0_blessed.nmf");
@@ -16,8 +14,6 @@ int test0()
 
 int test1()
 {
-	std::cout << "test1" << std::endl;
-
 	NMF::MAPPING mapping;
 	mapping.readFromFile("mapping1.nmf");
 	mapping.writeToFile("mapping1_blessed.nmf");
@@ -28,11 +24,13 @@ int main(int argc, char *argv[])
 {
 	int ret = 0;
 
-	ret = test0();
+    std::cout << "test0 ... " << std::endl;
+    ret = test0();
 	if (ret)
 		throw std::runtime_error("Error: " + std::to_string(ret));
 
-	ret = test1();
+    std::cout << "test1 ... " << std::endl;
+    ret = test1();
 	if (ret)
 		throw std::runtime_error("Error: " + std::to_string(ret));
 

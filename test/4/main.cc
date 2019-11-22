a09b25861f94459b480064d6c5617045e54f7177
Yu Cang <yhcy1993@gmail.com>
2019-11-22 16:11:08 +0800

    unify form

--- test/4/main.cc
+++ test/4/main.cc
@@ -1,47 +1,45 @@
 #include <iostream>
+#include <vector>
 #include "nmf.hpp"
 
-void test0()
+void LangleyExample()
 {
+	// 4 blocks in 2x2 form.
 	NMF::Mapping3D mapping("0/map.nmf");
 	mapping.summary();
 	mapping.writeToFile("0/map_blessed.nmf");
 }
 
-void test1()
+void TwoBlocks()
 {
 	NMF::Mapping3D mapping("1/map.nmf");
 	mapping.summary();
 	mapping.writeToFile("1/map_blessed.nmf");
 }
 
+typedef void(*pTestFunction)(void);
+const std::vector<pTestFunction> func = {
+	LangleyExample,
+	TwoBlocks
+};
+
 int main(int argc, char *argv[])
 {
-	std::cout << "test0 ... " << std::endl;
-	try {
-		test0();
-	}
-	catch (std::exception &e)
+	int cnt = 0;
+	for (auto f : func)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "\nCase " << ++cnt << " ..." << std::endl;
+		try {
+			f();
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		catch (...)
+		{
+			throw std::runtime_error("Unexpected.");
+		}
 	}
-	catch (...)
-	{
-		throw std::runtime_error("Unexpected.");
-	}
-
-	std::cout << "test1 ... " << std::endl;
-	try {
-		test1();
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	catch (...)
-	{
-		throw std::runtime_error("Unexpected.");
-	}
-
 	return 0;
 }

bcc736f4519cf26d3735b7e09030494af28f1221
Yu Cang <yhcy1993@gmail.com>
2019-12-11 15:48:09 +0800

    fix name of linear expansion

--- inc/spacing.h
+++ inc/spacing.h
@@ -14,13 +14,13 @@ namespace GridTool
 	{
 		std::vector<double> uniform(size_t n);
 		std::vector<double> uniform(double start, double end, size_t n);
+		std::vector<double> expansion(const std::vector<double> &seq, double start, double end);
 		std::vector<double> chebshev(double start, double end, size_t n);
 		std::vector<double> chebshev_multi(const std::vector<double> &seg, const std::vector<size_t> &n);
 		std::vector<double> single_exponential(size_t n, double a);
 		std::vector<double> double_exponential(size_t n, double a1, double a2, double a3);
 		std::vector<double> hyperbolic_tangent(size_t n, double b);
 		std::vector<double> hyperbolic_sine(size_t n, double c);
-		std::vector<double> linear_expand(const std::vector<double> &seq, double start, double end);
 	}
 }
 

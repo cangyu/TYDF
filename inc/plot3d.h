e0fc20a7c8f16f191738e21d4de30a2fe3d3e877
Yu Cang <yhcy1993@gmail.com>
2020-02-09 16:03:30 +0800

    separate constructors

--- inc/plot3d.h
+++ inc/plot3d.h
@@ -1,95 +1,56 @@
 #ifndef __GT_PLOT3D_H__
 #define __GT_PLOT3D_H__
 
-#include <fstream>
-#include <sstream>
 #include <vector>
-#include <string>
 #include "common.h"
 
 namespace GridTool
 {
-	namespace PLOT3D
-	{
-		using COMMON::Vector;
-		using COMMON::DIM;
-		using COMMON::ArrayND;
+    namespace PLOT3D
+    {
+        using COMMON::Vector;
+        using COMMON::DIM;
+        using COMMON::ArrayND;
 
-		class BLK : public DIM, public ArrayND<Vector>
-		{
-		public:
-			BLK(size_t nI, size_t nJ, bool is3D) :
-				DIM(2, is3D),
-				ArrayND<Vector>(nI, nJ, { 0.0, 0.0, 0.0 })
-			{
-				if (nI == 0)
-					throw std::invalid_argument("Invalid I dimension.");
-				if (nJ == 0)
-					throw std::invalid_argument("Invalid J dimension.");
-			}
-			BLK(size_t nI, size_t nJ, size_t nK) :
-				DIM(3),
-				ArrayND<Vector>(nI, nJ, nK, { 0.0, 0.0, 0.0 })
-			{
-				if (nI == 0)
-					throw std::invalid_argument("Invalid I dimension.");
-				if (nJ == 0)
-					throw std::invalid_argument("Invalid J dimension.");
-				if (nK == 0)
-					throw std::invalid_argument("Invalid K dimension.");
-			}
-			BLK(const BLK &rhs) = default;
-			~BLK() = default;
+        class BLK : public DIM, public ArrayND<Vector>
+        {
+        public:
+            BLK(size_t nI, size_t nJ, bool is3D);
+            BLK(size_t nI, size_t nJ, size_t nK);
+            BLK(const BLK &rhs) = default;
+            ~BLK() = default;
 
-			size_t node_num() const;
-			size_t cell_num() const;
-			size_t face_num() const;
+            size_t node_num() const;
+            size_t cell_num() const;
+            size_t face_num() const;
 
-			size_t boundary_face_num() const;
-			size_t internal_face_num() const;
-		};
+            size_t boundary_face_num() const;
+            size_t internal_face_num() const;
+        };
 
-		class GRID : public DIM
-		{
-		private:
-			std::vector<BLK *> m_blk;
+        class GRID : public DIM
+        {
+        private:
+            std::vector<BLK *> m_blk;
 
-		public:
-			GRID() : DIM(3), m_blk(0) {}
-			GRID(const std::string &fn) : DIM(3)
-			{
-				readFromFile(fn);
-			}
-			GRID(const GRID &rhs) :
-				DIM(rhs.dimension(), rhs.is3D()),
-				m_blk(rhs.m_blk.size(), nullptr)
-			{
-				for (size_t i = 0; i < numOfBlock(); ++i)
-					m_blk[i] = new BLK(*rhs.m_blk[i]);
-			}
-			~GRID()
-			{
-				release_all();
-			}
+        public:
+            GRID();
+            GRID(const std::string &fn);
+            GRID(const GRID &rhs);
+            ~GRID();
 
-			size_t numOfBlock() const { return m_blk.size(); }
+            size_t numOfBlock() const { return m_blk.size(); }
 
-			// IO
-			void readFromFile(const std::string &src);
-			void writeToFile(const std::string &dst) const;
+            // IO
+            void readFromFile(const std::string &src);
+            void writeToFile(const std::string &dst) const;
 
-			// 0-based indexing
-			BLK *block(size_t loc_idx) { return m_blk[loc_idx]; }
+            // 0-based indexing
+            BLK *block(size_t loc_idx) { return m_blk[loc_idx]; }
 
-		private:
-			void release_all()
-			{
-				for (auto e : m_blk)
-					if (e)
-						delete e;
-				m_blk.clear();
-			}
-		};
-	}
+        private:
+            void release_all();
+        };
+    }
 }
 #endif
--- src/plot3d.cc
+++ src/plot3d.cc
@@ -1,245 +1,293 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 #include "../inc/plot3d.h"
 
-static size_t cnt = 0;
-
-static void formatted_writer(std::ostream &fout, double val)
+static void formatted_writer(std::ostream &fout, double val, size_t &counter)
 {
-	static const size_t NumPerLine = 6;
-	static const std::string sep("\t");
+    static const size_t NumPerLine = 6;
+    static const std::string sep("\t");
 
-	fout << sep << val;
-	if (++cnt == NumPerLine)
-	{
-		fout << std::endl;
-		cnt = 0;
-	}
+    fout << sep << val;
+    if (++counter == NumPerLine)
+    {
+        fout << std::endl;
+        counter = 0;
+    }
 }
+
+struct invalid_dimension_size : public std::invalid_argument
+{
+    invalid_dimension_size(char dim, size_t n) : std::invalid_argument("Invalid " + std::to_string(dim) + " dimension: " + std::to_string(n) + ".") {}
+};
 
 namespace GridTool
 {
-	namespace PLOT3D
-	{
-		size_t BLK::node_num() const
-		{
-			if (is3D())
-				return nI()*nJ()*nK();
-			else
-				return nI()*nJ();
-		}
+    namespace PLOT3D
+    {
+        BLK::BLK(size_t nI, size_t nJ, bool is3D) : DIM(2, is3D), ArrayND<Vector>(nI, nJ, { 0.0, 0.0, 0.0 })
+        {
+            if (nI == 0)
+                throw invalid_dimension_size('I', nI);
+            if (nJ == 0)
+                throw invalid_dimension_size('J', nJ);
+        }
 
-		size_t BLK::cell_num() const
-		{
-			if (is3D())
-				return (nI() - 1)*(nJ() - 1)*(nK() - 1);
-			else
-				return (nI() - 1)*(nJ() - 1);
-		}
+        BLK::BLK(size_t nI, size_t nJ, size_t nK) : DIM(3), ArrayND<Vector>(nI, nJ, nK, { 0.0, 0.0, 0.0 })
+        {
+            if (nI == 0)
+                throw invalid_dimension_size('I', nI);
+            if (nJ == 0)
+                throw invalid_dimension_size('J', nJ);
+            if (nK == 0)
+                throw invalid_dimension_size('K', nK);
+        }
 
-		size_t BLK::face_num() const
-		{
-			size_t ret = 0;
-			if (is3D())
-			{
-				ret += (nI() - 1) * (nJ() - 1) * nK();
-				ret += (nJ() - 1) * (nK() - 1) * nI();
-				ret += (nK() - 1) * (nI() - 1) * nJ();
-			}
-			else
-			{
-				ret += (nI() - 1) * nJ();
-				ret += (nJ() - 1) * nI();
-			}
-			return ret;
-		}
+        size_t BLK::node_num() const
+        {
+            if (is3D())
+                return nI()*nJ()*nK();
+            else
+                return nI()*nJ();
+        }
 
-		size_t BLK::boundary_face_num() const
-		{
-			size_t ret = 0;
-			if (is3D())
-			{
-				ret += (nI() - 1) * (nJ() - 1);
-				ret += (nJ() - 1) * (nK() - 1);
-				ret += (nK() - 1) * (nI() - 1);
-			}
-			else
-			{
-				ret += (nI() - 1);
-				ret += (nJ() - 1);
-			}
-			ret *= 2;
-			return ret;
-		}
+        size_t BLK::cell_num() const
+        {
+            if (is3D())
+                return (nI() - 1)*(nJ() - 1)*(nK() - 1);
+            else
+                return (nI() - 1)*(nJ() - 1);
+        }
 
-		size_t BLK::internal_face_num() const
-		{
-			return face_num() - boundary_face_num();
-		}
+        size_t BLK::face_num() const
+        {
+            size_t ret = 0;
+            if (is3D())
+            {
+                ret += (nI() - 1) * (nJ() - 1) * nK();
+                ret += (nJ() - 1) * (nK() - 1) * nI();
+                ret += (nK() - 1) * (nI() - 1) * nJ();
+            }
+            else
+            {
+                ret += (nI() - 1) * nJ();
+                ret += (nJ() - 1) * nI();
+            }
+            return ret;
+        }
 
-		void GRID::readFromFile(const std::string &src)
-		{
-			std::string s;
-			std::stringstream ss;
+        size_t BLK::boundary_face_num() const
+        {
+            size_t ret = 0;
+            if (is3D())
+            {
+                ret += (nI() - 1) * (nJ() - 1);
+                ret += (nJ() - 1) * (nK() - 1);
+                ret += (nK() - 1) * (nI() - 1);
+            }
+            else
+            {
+                ret += (nI() - 1);
+                ret += (nJ() - 1);
+            }
+            ret *= 2;
+            return ret;
+        }
 
-			// Open input grid file.
-			std::ifstream fin(src);
-			if (!fin)
-				throw std::runtime_error("Failed to read the input grid.");
+        size_t BLK::internal_face_num() const
+        {
+            return face_num() - boundary_face_num();
+        }
 
-			// Read block num.
-			std::getline(fin, s);
-			ss << s;
-			int blk_num = 0;
-			ss >> blk_num;
-			if (blk_num <= 0)
-				throw std::invalid_argument("Invalid num of blocks.");
+        GRID::GRID() : DIM(3), m_blk(0) {}
 
-			// Drop previous contents.
-			release_all();
+        GRID::GRID(const std::string &fn) : DIM(3) { readFromFile(fn); }
 
-			// Read dimensions of each block,and allocate new storage.
-			m_blk.resize(blk_num, nullptr);
-			for (int n = 0; n < blk_num; ++n)
-			{
-				int IMAX = 0, JMAX = 0, KMAX = 0;
-				std::getline(fin, s);
-				ss.clear();
-				ss << s;
-				ss >> IMAX >> JMAX;
-				if (IMAX <= 0)
-					throw std::invalid_argument("Invalid I dimension of Block " + std::to_string(n + 1) + ".");
-				if (JMAX <= 0)
-					throw std::invalid_argument("Invalid J dimension of Block " + std::to_string(n + 1) + ".");
+        GRID::GRID(const GRID &rhs) :
+            DIM(rhs.dimension(), rhs.is3D()),
+            m_blk(rhs.m_blk.size(), nullptr)
+        {
+            for (size_t i = 0; i < numOfBlock(); ++i)
+                m_blk[i] = new BLK(*rhs.m_blk[i]);
+        }
 
-				if (!(ss >> KMAX))
-					m_blk[n] = new BLK((size_t)IMAX, (size_t)JMAX, false);
-				else if (KMAX == 1)
-					m_blk[n] = new BLK((size_t)IMAX, (size_t)JMAX, true);
-				else
-				{
-					if (JMAX <= 0)
-						throw std::invalid_argument("Invalid K dimension of Block " + std::to_string(n + 1) + ".");
-					else
-						m_blk[n] = new BLK((size_t)IMAX, (size_t)JMAX, (size_t)KMAX);
-				}
-			}
+        GRID::~GRID() { release_all(); }
 
-			// Read coordinates of each block.
-			for (auto b : m_blk)
-			{
-				const size_t NX = b->nI(), NY = b->nJ(), NZ = b->nK();
-				if (b->dimension() == 3)
-				{
-					for (size_t k = 0; k < NZ; ++k)
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								fin >> b->at(i, j, k).x();
+        void GRID::readFromFile(const std::string &src)
+        {
+            std::string s;
+            std::stringstream ss;
 
-					for (size_t k = 0; k < NZ; ++k)
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								fin >> b->at(i, j, k).y();
+            // Open input grid file.
+            std::ifstream fin(src);
+            if (!fin)
+                throw std::runtime_error("Failed to read the input grid.");
 
-					for (size_t k = 0; k < NZ; ++k)
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								fin >> b->at(i, j, k).z();
-				}
-				else
-				{
-					for (size_t j = 0; j < NY; ++j)
-						for (size_t i = 0; i < NX; ++i)
-							fin >> b->at(i, j).x();
+            // Read block num.
+            std::getline(fin, s);
+            ss << s;
+            int blk_num = 0;
+            ss >> blk_num;
+            if (blk_num <= 0)
+                throw std::invalid_argument("Invalid num of blocks.");
 
-					for (size_t j = 0; j < NY; ++j)
-						for (size_t i = 0; i < NX; ++i)
-							fin >> b->at(i, j).y();
+            // Drop previous contents.
+            release_all();
 
-					if (b->is3D())
-					{
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								fin >> b->at(i, j).z();
-					}
-				}
-			}
+            // Read dimensions of each block,and allocate new storage.
+            m_blk.resize(blk_num, nullptr);
+            for (int n = 0; n < blk_num; ++n)
+            {
+                int IMAX = 0, JMAX = 0, KMAX = 0;
+                std::getline(fin, s);
+                ss.clear();
+                ss << s;
+                ss >> IMAX >> JMAX;
+                if (IMAX <= 0)
+                    throw std::invalid_argument("Invalid I dimension of Block " + std::to_string(n + 1) + ".");
+                if (JMAX <= 0)
+                    throw std::invalid_argument("Invalid J dimension of Block " + std::to_string(n + 1) + ".");
 
-			// Close file.
-			fin.close();
+                if (!(ss >> KMAX))
+                    m_blk[n] = new BLK((size_t)IMAX, (size_t)JMAX, false);
+                else if (KMAX == 1)
+                    m_blk[n] = new BLK((size_t)IMAX, (size_t)JMAX, true);
+                else
+                {
+                    if (JMAX <= 0)
+                        throw std::invalid_argument("Invalid K dimension of Block " + std::to_string(n + 1) + ".");
+                    else
+                        m_blk[n] = new BLK((size_t)IMAX, (size_t)JMAX, (size_t)KMAX);
+                }
+            }
 
-			// Update grid global DIM attributes, and check dimension consistency.
-			m_is3D = m_blk[0]->is3D();
-			m_dim = m_blk[0]->dimension();
-			for (size_t n = 1; n < m_blk.size(); ++n)
-			{
-				auto blk = m_blk[n];
-				if (blk->is3D() != m_is3D || blk->dimension() != m_dim)
-					throw std::runtime_error("Inconsistent DIM properties of Block " + std::to_string(n + 1) + ".");
-			}
-		}
+            // Read coordinates of each block.
+            for (auto b : m_blk)
+            {
+                const size_t NX = b->nI(), NY = b->nJ(), NZ = b->nK();
+                if (b->dimension() == 3)
+                {
+                    for (size_t k = 0; k < NZ; ++k)
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                fin >> b->at(i, j, k).x();
 
-		void GRID::writeToFile(const std::string &dst) const
-		{
-			// Open output file.
-			std::ofstream fout(dst);
-			if (!fout)
-				throw std::runtime_error("Failed to open the target output grid file.");
+                    for (size_t k = 0; k < NZ; ++k)
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                fin >> b->at(i, j, k).y();
 
-			// Write num of blocks.
-			fout << "\t" << numOfBlock() << std::endl;
+                    for (size_t k = 0; k < NZ; ++k)
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                fin >> b->at(i, j, k).z();
+                }
+                else
+                {
+                    for (size_t j = 0; j < NY; ++j)
+                        for (size_t i = 0; i < NX; ++i)
+                            fin >> b->at(i, j).x();
 
-			// Write dimensions of each block.
-			for (auto b : m_blk)
-			{
-				fout << "\t" << b->nI();
-				fout << "\t" << b->nJ();
-				if (b->is3D())
-					fout << "\t" << b->nK();
-				fout << std::endl;
-			}
+                    for (size_t j = 0; j < NY; ++j)
+                        for (size_t i = 0; i < NX; ++i)
+                            fin >> b->at(i, j).y();
 
-			// Write coordinates of each block
-			cnt = 0;
-			for (auto b : m_blk)
-			{
-				const size_t NX = b->nI(), NY = b->nJ(), NZ = b->nK();
-				if (b->dimension() == 3)
-				{
-					for (size_t k = 0; k < NZ; ++k)
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								formatted_writer(fout, b->at(i, j, k).x());
+                    if (b->is3D())
+                    {
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                fin >> b->at(i, j).z();
+                    }
+                }
+            }
 
-					for (size_t k = 0; k < NZ; ++k)
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								formatted_writer(fout, b->at(i, j, k).y());
+            // Close file.
+            fin.close();
 
-					for (size_t k = 0; k < NZ; ++k)
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								formatted_writer(fout, b->at(i, j, k).z());
-				}
-				else
-				{
-					for (size_t j = 0; j < NY; ++j)
-						for (size_t i = 0; i < NX; ++i)
-							formatted_writer(fout, b->at(i, j).x());
+            // Update grid global DIM attributes, and check dimension consistency.
+            m_is3D = m_blk[0]->is3D();
+            m_dim = m_blk[0]->dimension();
+            for (size_t n = 1; n < m_blk.size(); ++n)
+            {
+                auto blk = m_blk[n];
+                if (blk->is3D() != m_is3D || blk->dimension() != m_dim)
+                    throw std::runtime_error("Inconsistent DIM properties of Block " + std::to_string(n + 1) + ".");
+            }
+        }
 
-					for (size_t j = 0; j < NY; ++j)
-						for (size_t i = 0; i < NX; ++i)
-							formatted_writer(fout, b->at(i, j).y());
+        void GRID::writeToFile(const std::string &dst) const
+        {
+            // Open output file.
+            std::ofstream fout(dst);
+            if (!fout)
+                throw std::runtime_error("Failed to open the target output grid file.");
 
-					if (b->is3D())
-					{
-						for (size_t j = 0; j < NY; ++j)
-							for (size_t i = 0; i < NX; ++i)
-								formatted_writer(fout, b->at(i, j).z());
-					}
-				}
-			}
+            // Write num of blocks.
+            fout << "\t" << numOfBlock() << std::endl;
 
-			// Close file.
-			fout.close();
-		}
-	}
+            // Write dimensions of each block.
+            for (auto b : m_blk)
+            {
+                fout << "\t" << b->nI();
+                fout << "\t" << b->nJ();
+                if (b->is3D())
+                    fout << "\t" << b->nK();
+                fout << std::endl;
+            }
+
+            // Write coordinates of each block
+            size_t cnt = 0;
+            for (auto b : m_blk)
+            {
+                const size_t NX = b->nI(), NY = b->nJ(), NZ = b->nK();
+                if (b->dimension() == 3)
+                {
+                    for (size_t k = 0; k < NZ; ++k)
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                formatted_writer(fout, b->at(i, j, k).x(), cnt);
+
+                    for (size_t k = 0; k < NZ; ++k)
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                formatted_writer(fout, b->at(i, j, k).y(), cnt);
+
+                    for (size_t k = 0; k < NZ; ++k)
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                formatted_writer(fout, b->at(i, j, k).z(), cnt);
+                }
+                else
+                {
+                    for (size_t j = 0; j < NY; ++j)
+                        for (size_t i = 0; i < NX; ++i)
+                            formatted_writer(fout, b->at(i, j).x(), cnt);
+
+                    for (size_t j = 0; j < NY; ++j)
+                        for (size_t i = 0; i < NX; ++i)
+                            formatted_writer(fout, b->at(i, j).y(), cnt);
+
+                    if (b->is3D())
+                    {
+                        for (size_t j = 0; j < NY; ++j)
+                            for (size_t i = 0; i < NX; ++i)
+                                formatted_writer(fout, b->at(i, j).z(), cnt);
+                    }
+                }
+            }
+
+            // Close file.
+            fout.close();
+        }
+
+        void GRID::release_all()
+        {
+            for (auto e : m_blk)
+                if (e)
+                    delete e;
+
+            m_blk.clear();
+        }
+    }
 }

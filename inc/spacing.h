17e1761e98c1c733418389fda4aa308076f7c969
Yu Cang <yhcy1993@gmail.com>
2020-02-09 15:38:56 +0800

    spacing utilities done

--- inc/spacing.h
+++ inc/spacing.h
@@ -2,27 +2,23 @@
 #define __GT_SPACING_H__
 
 #include <vector>
-#include <cstddef>
 
-/*
-	All nodes distribute through [0, 1] by default unless specified.
-*/
+typedef std::vector<double> DIST_ARR;
 
 namespace GridTool
 {
-	namespace SPACING
-	{
-		int uniform(size_t n, std::vector<double> &dst);
-		int uniform(double start, double end, size_t n, std::vector<double> &dst);
-
-		std::vector<double> expansion(const std::vector<double> &seq, double start, double end);
-		std::vector<double> chebshev(double start, double end, size_t n);
-		std::vector<double> chebshev_multi(const std::vector<double> &seg, const std::vector<size_t> &n);
-		std::vector<double> single_exponential(size_t n, double a);
-		std::vector<double> double_exponential(size_t n, double a1, double a2, double a3);
-		std::vector<double> hyperbolic_tangent(size_t n, double b);
-		std::vector<double> hyperbolic_sine(size_t n, double c);
-	}
+    namespace SPACING
+    {
+        /// All nodes distribute through [0, 1] by default unless specified.
+        void uniform(int n, DIST_ARR &dst);
+        void uniform(double a, double b, int n, DIST_ARR &dst);
+        void chebshev(double a, double b, int n, DIST_ARR &dst);
+        void chebshev(const std::vector<double> &seg, const std::vector<int> &num, DIST_ARR &dst);
+        void single_exponential(int n, double a, DIST_ARR &dst);
+        void double_exponential(int n, double a1, double a2, double a3, DIST_ARR &dst);
+        void hyperbolic_tangent(int n, double b, DIST_ARR &dst);
+        void hyperbolic_sine(int n, double c, DIST_ARR &dst);
+    }
 }
 
 #endif
--- src/spacing.cc
+++ src/spacing.cc
@@ -1,93 +1,219 @@
 #include <cmath>
-#include <algorithm>
+#include <cstddef>
+#include <stdexcept>
+#include <string>
 #include "../inc/common.h"
 #include "../inc/spacing.h"
 
+struct invalid_node_num : public std::invalid_argument
+{
+    explicit invalid_node_num(int n) : std::invalid_argument("At least 2 nodes are required, got \"" + std::to_string(n) + "\".") {}
+};
+
 namespace GridTool
 {
-	namespace SPACING
-	{
-		using COMMON::relaxation;
+    namespace SPACING
+    {
+        using COMMON::relaxation;
 
-		int uniform(size_t n, std::vector<double> &dst)
-		{
-			if (n == 0)
-				return -1;
-			if (n == 1)
-				return -2;
+        /**
+         * Uniform distribution through [0,1].
+         * @param n Num of nodes.
+         * @param dst Target distribution.
+         */
+        void uniform(int n, DIST_ARR &dst)
+        {
+            if (n < 2)
+                throw invalid_node_num(n);
 
-			dst.resize(n);
-			for (size_t i = 0; i < n; ++i)
-			{
-				const double ratio = 1.0 * i / (n - 1);
-				dst[i] = relaxation(0, 1, ratio);
-			}
-			return 0;
-		}
+            if (dst.size() != n)
+                dst.resize(n);
 
-		int uniform(double start, double end, size_t n, std::vector<double> &dst)
-		{
-			if (n == 0)
-				return -1;
-			if (n == 1)
-				return -2;
+            for (int i = 0; i < n; ++i)
+                dst[i] = 1.0 * i / (n - 1);
+        }
 
-			dst.resize(n);
-			for (size_t i = 0; i < n; ++i)
-			{
-				const double ratio = 1.0 * i / (n - 1);
-				dst[i] = relaxation(start, end, ratio);
-			}
-			return 0;
-		}
+        /**
+         * Uniform distribution through [a,b].
+         * @param a The starting position.
+         * @param b The ending position.
+         * @param n Num of nodes.
+         * @param dst Target distribution.
+         */
+        void uniform(double a, double b, int n, DIST_ARR &dst)
+        {
+            uniform(n, dst);
 
-		std::vector<double> expansion(const std::vector<double> &seq, double start, double end)
-		{
-			std::vector<double> ret;
+            for (auto &e : dst)
+                e = relaxation(a, b, e);
+        }
 
-			return ret;
-		}
+        /**
+         * Chebshev distribution through [a,b].
+         * @param a The starting position.
+         * @param b The ending position.
+         * @param n Num of nodes.
+         * @param dst Target distribution.
+         */
+        void chebshev(double a, double b, int n, DIST_ARR &dst)
+        {
+            static const double pi = std::acos(-1.0);
 
-		std::vector<double> chebshev(double start, double end, size_t n)
-		{
-			std::vector<double> ret;
+            uniform(pi, 0.0, n, dst);
 
-			return ret;
-		}
+            for (auto &e : dst)
+            {
+                const auto ratio = 0.5 * (1.0 + std::cos(e));
+                e = relaxation(a, b, ratio);
+            }
+        }
 
-		std::vector<double> chebshev_multi(const std::vector<double> &seg, const std::vector<size_t> &n)
-		{
-			std::vector<double> ret;
+        /**
+         * Multi-segment Chebshev distribution.
+         * @param seg Splitting values.
+         * @param num Num of nodes within each segment.
+         * @param dst Target distribution.
+         */
+        void chebshev(const std::vector<double> &seg, const std::vector<int> &num, DIST_ARR &dst)
+        {
+            if (seg.size() < 2)
+                throw std::invalid_argument("Too less splitting values.");
+            if (seg.size() != num.size())
+                throw std::invalid_argument("Inconsistent splitting intervals.");
 
-			return ret;
-		}
+            int n = 1;
+            for (auto e : num)
+            {
+                if (e < 2)
+                    throw invalid_node_num(e);
 
-		std::vector<double> single_exponential(size_t n, double a)
-		{
-			std::vector<double> ret;
+                n += (e - 1);
+            }
 
-			return ret;
-		}
+            if (dst.size() != n)
+                dst.resize(n);
 
-		std::vector<double> double_exponential(size_t n, double a1, double a2, double a3)
-		{
-			std::vector<double> ret;
+            dst[0] = seg[0];
+            size_t pos = 1;
 
-			return ret;
-		}
+            for (size_t i = 0; i < num.size(); ++i)
+            {
+                const auto cn = num[i];
+                std::vector<double> ccd(cn, 0.0);
+                chebshev(seg[i], seg[i + 1], cn, ccd);
+                for (int j = 1; j < cn; ++j)
+                    dst[pos++] = ccd[j];
+            }
 
-		std::vector<double> hyperbolic_tangent(size_t n, double b)
-		{
-			std::vector<double> ret;
+            if (pos != n)
+                throw std::runtime_error("Unexpected terminating position.");
+        }
 
-			return ret;
-		}
+        /**
+         * Single exponential distribution of n nodes in [0, 1].
+         * @param n Number of nodes.
+         * @param a The control parameter.
+         *          For a>0, nodes aggregate towards the starting position;
+         *          For a=0, nodes are uniform, no aggregation;
+         *          For a<0, nodes aggregate towards the ending position.
+         * @param dst Target distribution.
+         */
+        void single_exponential(int n, double a, DIST_ARR &dst)
+        {
+            uniform(n, dst);
 
-		std::vector<double> hyperbolic_sine(size_t n, double c)
-		{
-			std::vector<double> ret;
+            if (std::abs(a) < 1e-12)
+                throw std::invalid_argument("\"a\" shouldn't be 0.");
+            else
+            {
+                const double t = std::exp(a) - 1.0;
+                for (auto &e : dst)
+                    e = (std::exp(a * e) - 1.0) / t;
+            }
+        }
 
-			return ret;
-		}
-	}
+        /**
+         * Double exponential distribution of n nodes in [0, 1].
+         * The curve will go through (a3, a1).
+         * @param n Number of nodes.
+         * @param a1 Horizontal coordinate of the control point.
+         * @param a2 The control parameter.
+         *           For a2>0, nodes aggregate towards boundary;
+         *			 For a2=0, nodes are uniform, no aggregation;
+         *           For a2<0, nodes aggregate towards center.
+         * @param a3 Vertical coordinate of the control point.
+         * @param dst Target distribution.
+         */
+        void double_exponential(int n, double a1, double a2, double a3, DIST_ARR &dst)
+        {
+            uniform(n, dst);
+
+            if (std::abs(a2) < 1e-12)
+                throw std::invalid_argument("\"a2\" shouldn't be 0.");
+
+            const double p = (1 - a1) * a3 * (std::exp(a2) - 1) / ((1 - a3) * a1 * a2 * std::exp(a2));
+            if (p <= 0.0)
+                throw std::invalid_argument("\"p\" should be positive.");
+
+            // Solve "a4" using Newton-Raphson iteration.
+            static const size_t MAX_ITER = 50;
+            static const double PRECISION = -6.0;
+            double a4 = 5 * std::log(p);
+            bool ok = false;
+            for (size_t i = 0; i < MAX_ITER; ++i)
+            {
+                const double f = std::exp(a4) - 1.0 - p * a4;
+                if (std::log10(std::abs(f)) < PRECISION)
+                {
+                    ok = true;
+                    break;
+                }
+
+                const double df = std::exp(a4) - p;
+                a4 -= f / df;
+            }
+            if (!ok)
+                throw std::runtime_error("Newton-Raphson iteration failed to converge.");
+
+            const double ea21 = std::exp(a2) - 1.0;
+            const double ea41 = std::exp(a4) - 1.0;
+            for (auto &e : dst)
+            {
+                if (e <= a3)
+                    e = a1 * (std::exp(a2 / a3 * e) - 1.0) / ea21;
+                else
+                    e = a1 + (1.0 - a1) * (std::exp(a4 / (1.0 - a3) * (e - a3)) - 1.0) / ea41;
+            }
+        }
+
+        /**
+         *  Hyperbolic tangent distribution of n nodes in [0, 1].
+         * @param n Number of nodes.
+         * @param b The control parameter.
+         * @param dst Target distribution.
+         */
+        void hyperbolic_tangent(int n, double b, DIST_ARR &dst)
+        {
+            uniform(n, dst);
+
+            const double tb = std::tanh(b);
+            for (auto &e : dst)
+                e = 1.0 + std::tanh(b * (e - 1.0)) / tb;
+        }
+
+        /**
+         *  Hyperbolic sine distribution of n nodes in [0, 1].
+         * @param n Number of nodes.
+         * @param c The control parameter.
+         * @param dst Target distribution.
+         */
+        void hyperbolic_sine(int n, double c, DIST_ARR &dst)
+        {
+            uniform(n, dst);
+
+            const double sc = std::sinh(c);
+            for (auto &e : dst)
+                e = 1.0 + std::sinh(c * (e - 1.0)) / sc;
+        }
+    }
 }

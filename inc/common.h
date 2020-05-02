b6ba46d942cf8a13b382bb5b9502eaa34a8eb327
Yu Cang <yhcy1993@gmail.com>
2020-05-02 16:27:42 +0800

    adjust comment format and namespace style

--- inc/common.h
+++ inc/common.h
@@ -1,5 +1,5 @@
-#ifndef __GT_COMMON_H__
-#define __GT_COMMON_H__
+#ifndef TYDF_COMMON_H
+#define TYDF_COMMON_H
 
 #include <cmath>
 #include <array>
@@ -8,239 +8,308 @@
 #include <exception>
 #include <stdexcept>
 
-namespace GridTool
+namespace GridTool::COMMON
 {
-	namespace COMMON
-	{
-		double relaxation(double a, double b, double x);
+    typedef double Scalar;
 
-		struct wrong_index : public std::logic_error
-		{
-			wrong_index(long long idx, const std::string &reason) : std::logic_error("\"" + std::to_string(idx) + "\" " + reason + ".") {}
-			virtual ~wrong_index() = default;
-		};
+    Scalar relaxation(Scalar a, Scalar b, Scalar x);
 
-		class DIM
-		{
-		private:
-			struct wrong_dimension : public wrong_index
-			{
-				wrong_dimension(int dim) : wrong_index(dim, "is not a valid dimension") {}
-			};
+    struct wrong_index : public std::logic_error
+    {
+        wrong_index(long long idx, const std::string &reason) : std::logic_error("\"" + std::to_string(idx) + "\" " + reason + ".") {}
+        virtual ~wrong_index() = default;
+    };
 
-		protected:
-			bool m_is3D;
-			int m_dim;
+    class DIM
+    {
+    private:
+        struct wrong_dimension : public wrong_index { wrong_dimension(int dim) : wrong_index(dim, "is not a valid dimension") {} };
 
-		public:
-			DIM() = delete;
-			DIM(int dim, bool is3d = true);
-			DIM(const DIM &rhs) = default;
-			virtual ~DIM() = default;
+    protected:
+        bool m_is3D;
+        int m_dim;
 
-			bool is3D() const { return m_is3D; }
-			int dimension() const { return m_dim; }
-		};
+    public:
+        DIM() = delete;
 
-		typedef double Scalar;
+        DIM(int dim, bool is3d = true);
 
-		class Vector : public std::array<Scalar, 3>
-		{
-		protected:
-			struct not_vector_component : public wrong_index
-			{
-				not_vector_component(short x) : wrong_index(x, "is not a valid index of certain vector component") {}
-			};
+        DIM(const DIM &rhs) = default;
 
-		public:
-			Vector() : std::array<Scalar, 3>{0.0, 0.0, 0.0} {}
-			Vector(Scalar val) : std::array<Scalar, 3>{val, val, val} {}
-			Vector(Scalar v1, Scalar v2, Scalar v3) : std::array<Scalar, 3>{v1, v2, v3} {}
-			Vector(const Vector &obj) : std::array<Scalar, 3>{obj.x(), obj.y(), obj.z()} {}
-			~Vector() = default;
+        virtual ~DIM() = default;
 
-			// 1-based indexing
-			const Scalar &operator()(short idx) const;
-			Scalar &operator()(short idx);
+        bool is3D() const;
 
-			// Access through component
-			const Scalar &x() const { return at(0); }
-			const Scalar &y() const { return at(1); }
-			const Scalar &z() const { return at(2); }
-			Scalar &x() { return at(0); }
-			Scalar &y() { return at(1); }
-			Scalar &z() { return at(2); }
+        int dimension() const;
+    };
 
-			// Operators
-			Vector &operator=(const Vector &rhs);
-			Vector &operator+=(const Vector &rhs);
-			Vector &operator-=(const Vector &rhs);
-			Vector &operator*=(Scalar a);
-			Vector &operator/=(Scalar a);
+    class Vector : public std::array<Scalar, 3>
+    {
+    protected:
+        struct not_vector_component : public wrong_index { not_vector_component(short x) : wrong_index(x, "is not a valid index of certain vector component") {} };
 
-			// Mathematical operations
-			Scalar dot(const Vector &b) const;
-			Vector cross(const Vector &b) const;
-			Scalar norm() const;
-			void normalize();
-		};
+    public:
+        Vector() : std::array<Scalar, 3>{0.0, 0.0, 0.0} {}
 
-		template <typename T>
-		class Array1D : public std::vector<T>
-		{
-		private:
-			struct index_is_zero : public std::invalid_argument
-			{
-				index_is_zero() : std::invalid_argument("0 is invalid when using 1-based index.") {}
-			};
+        Vector(Scalar val) : std::array<Scalar, 3>{val, val, val} {}
 
-		public:
-			Array1D() : std::vector<T>() {}
-			explicit Array1D(size_t n) : std::vector<T>(n) {}
-			Array1D(size_t n, const T &val) : std::vector<T>(n, val) {}
-			Array1D(const Array1D &obj) = default;
-			~Array1D() = default;
+        Vector(Scalar v1, Scalar v2, Scalar v3) : std::array<Scalar, 3>{v1, v2, v3} {}
 
-			// 1-based indexing
-			T &operator()(long long i)
-			{
-				if (i >= 1)
-					return std::vector<T>::at(i - 1);
-				else if (i <= -1)
-					return std::vector<T>::at(std::vector<T>::size() + i);
-				else
-					throw index_is_zero();
-			}
-			const T &operator()(long long i) const
-			{
-				if (i >= 1)
-					return std::vector<T>::at(i - 1);
-				else if (i <= -1)
-					return std::vector<T>::at(std::vector<T>::size() + i);
-				else
-					throw index_is_zero();
-			}
+        Vector(const Vector &obj) : std::array<Scalar, 3>{obj.x(), obj.y(), obj.z()} {}
 
-			// Check includances
-			bool contains(const T &x) const
-			{
-				const size_t N = this->size();
-				for (size_t i = 0; i < N; ++i)
-					if (x == this->at(i))
-						return true;
+        ~Vector() = default;
 
-				return false;
-			}
-			bool contains(const T &a, const T &b) const
-			{
-				bool flag_a = false, flag_b = false;
-				const size_t N = this->size();
-				for (size_t i = 0; i < N; ++i)
-				{
-					const T &x = this->at(i);
-					if (!flag_a && a == x)
-						flag_a = true;
-					if (!flag_b && b == x)
-						flag_b = true;
+        /// 1-based indexing
+        const Scalar &operator()(short idx) const;
 
-					if (flag_a && flag_b)
-						return true;
-				}
-				return false;
-			}
-		};
+        Scalar &operator()(short idx);
 
-		template<typename T>
-		class ArrayND
-		{
-		protected:
-			size_t m_Nx, m_Ny, m_Nz;
-			std::vector<T> m_data;
-			size_t m_NXY;
+        /// Access through component
+        const Scalar &x() const;
 
-		public:
-			ArrayND() = delete;
-			ArrayND(size_t nx, const T &val) :
-				m_Nx(nx),
-				m_Ny(1),
-				m_Nz(1),
-				m_data(nx, val),
-				m_NXY(nx)
-			{
-				if (nI() == 0)
-					throw std::invalid_argument("0 in I-dim.");
-			}
-			ArrayND(size_t nx, size_t ny, const T &val) :
-				m_Nx(nx),
-				m_Ny(ny),
-				m_Nz(1),
-				m_data(nx*ny, val),
-				m_NXY(nx*ny)
-			{
-				if (nI() == 0)
-					throw std::invalid_argument("0 in I-dim.");
-				if (nJ() == 0)
-					throw std::invalid_argument("0 in J-dim.");
-			}
-			ArrayND(size_t nx, size_t ny, size_t nz, const T &val) :
-				m_Nx(nx),
-				m_Ny(ny),
-				m_Nz(nz),
-				m_data(nx*ny*nz, val),
-				m_NXY(nx*ny)
-			{
-				if (nI() == 0)
-					throw std::invalid_argument("0 in I-dim.");
-				if (nJ() == 0)
-					throw std::invalid_argument("0 in J-dim.");
-				if (nK() == 0)
-					throw std::invalid_argument("0 in K-dim.");
-			}
-			ArrayND(const ArrayND &rhs) :
-				m_Nx(rhs.m_Nx),
-				m_Ny(rhs.m_Ny),
-				m_Nz(rhs.m_Nz),
-				m_data(rhs.m_data.begin(), rhs.m_data.end()),
-				m_NXY(rhs.m_NXY)
-			{
-				if (nI() == 0)
-					throw std::runtime_error("0 in I-dim.");
-				if (nJ() == 0)
-					throw std::runtime_error("0 in J-dim.");
-				if (nK() == 0)
-					throw std::runtime_error("0 in K-dim.");
-			}
-			virtual ~ArrayND() = default;
+        const Scalar &y() const;
 
-			size_t nI() const { return m_Nx; }
-			size_t nJ() const { return m_Ny; }
-			size_t nK() const { return m_Nz; }
+        const Scalar &z() const;
 
-		private:
-			// Calculate 0-based internal index
-			size_t idx(size_t i, size_t j) const { return i + m_Nx * j; }
-			size_t idx(size_t i, size_t j, size_t k) const { return i + m_Nx * j + m_NXY * k; }
+        Scalar &x();
 
-		public:
-			/********************************* 2D *********************************/
-			// 0-based indexing
-			T& at(size_t i, size_t j) { return m_data[idx(i, j)]; }
-			T at(size_t i, size_t j) const { return m_data[idx(i, j)]; }
+        Scalar &y();
 
-			// 1-based indexing
-			T& operator()(size_t i, size_t j) { return at(i - 1, j - 1); }
-			T operator()(size_t i, size_t j) const { return at(i - 1, j - 1); }
+        Scalar &z();
 
-			/********************************* 3D *********************************/
-			// 0-based indexing
-			T& at(size_t i, size_t j, size_t k) { return m_data[idx(i, j, k)]; }
-			T at(size_t i, size_t j, size_t k) const { return m_data[idx(i, j, k)]; }
+        /// Operators
+        Vector &operator=(const Vector &rhs);
 
-			// 1-based indexing
-			T& operator()(size_t i, size_t j, size_t k) { return at(i - 1, j - 1, k - 1); }
-			T operator()(size_t i, size_t j, size_t k) const { return at(i - 1, j - 1, k - 1); }
-		};
-	}
+        Vector &operator+=(const Vector &rhs);
+
+        Vector &operator-=(const Vector &rhs);
+
+        Vector &operator*=(Scalar a);
+
+        Vector &operator/=(Scalar a);
+
+        /// Mathematical operations
+        Scalar dot(const Vector &b) const;
+
+        Vector cross(const Vector &b) const;
+
+        Scalar norm() const;
+
+        void normalize();
+    };
+
+    template <typename T>
+    class Array1D : public std::vector<T>
+    {
+    private:
+        struct index_is_zero : public std::invalid_argument { index_is_zero() : std::invalid_argument("0 is invalid when using 1-based index.") {} };
+
+    public:
+        Array1D() : std::vector<T>() {}
+
+        explicit Array1D(size_t n) : std::vector<T>(n) {}
+
+        Array1D(size_t n, const T &val) : std::vector<T>(n, val) {}
+
+        Array1D(const Array1D &obj) = default;
+
+        ~Array1D() = default;
+
+        /// 1-based indexing
+        const T &operator()(int i) const
+        {
+            if (i >= 1)
+                return std::vector<T>::at(i - 1);
+            else if (i <= -1)
+                return std::vector<T>::at(std::vector<T>::size() + i);
+            else
+                throw index_is_zero();
+        }
+
+        T &operator()(int i)
+        {
+            if (i >= 1)
+                return std::vector<T>::at(i - 1);
+            else if (i <= -1)
+                return std::vector<T>::at(std::vector<T>::size() + i);
+            else
+                throw index_is_zero();
+        }
+
+        /// Check includances
+        bool contains(const T &x) const
+        {
+            const size_t N = this->size();
+            for (size_t i = 0; i < N; ++i)
+                if (x == this->at(i))
+                    return true;
+
+            return false;
+        }
+        bool contains(const T &a, const T &b) const
+        {
+            bool flag_a = false, flag_b = false;
+            const size_t N = this->size();
+            for (size_t i = 0; i < N; ++i)
+            {
+                const T &x = this->at(i);
+                if (!flag_a && a == x)
+                    flag_a = true;
+                if (!flag_b && b == x)
+                    flag_b = true;
+
+                if (flag_a && flag_b)
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    template<typename T>
+    class ArrayND
+    {
+    protected:
+        size_t m_Nx, m_Ny, m_Nz;
+        std::vector<T> m_data;
+        size_t m_NXY;
+
+    public:
+        ArrayND() = delete;
+
+        ArrayND(size_t nx, const T &val) :
+            m_Nx(nx),
+            m_Ny(1),
+            m_Nz(1),
+            m_data(nx, val),
+            m_NXY(nx)
+        {
+            if (nI() == 0)
+                throw std::invalid_argument("0 in I-dim.");
+        }
+
+        ArrayND(size_t nx, size_t ny, const T &val) :
+            m_Nx(nx),
+            m_Ny(ny),
+            m_Nz(1),
+            m_data(nx*ny, val),
+            m_NXY(nx*ny)
+        {
+            if (nI() == 0)
+                throw std::invalid_argument("0 in I-dim.");
+            if (nJ() == 0)
+                throw std::invalid_argument("0 in J-dim.");
+        }
+
+        ArrayND(size_t nx, size_t ny, size_t nz, const T &val) :
+            m_Nx(nx),
+            m_Ny(ny),
+            m_Nz(nz),
+            m_data(nx*ny*nz, val),
+            m_NXY(nx*ny)
+        {
+            if (nI() == 0)
+                throw std::invalid_argument("0 in I-dim.");
+            if (nJ() == 0)
+                throw std::invalid_argument("0 in J-dim.");
+            if (nK() == 0)
+                throw std::invalid_argument("0 in K-dim.");
+        }
+
+        ArrayND(const ArrayND &rhs) :
+            m_Nx(rhs.m_Nx),
+            m_Ny(rhs.m_Ny),
+            m_Nz(rhs.m_Nz),
+            m_data(rhs.m_data.begin(), rhs.m_data.end()),
+            m_NXY(rhs.m_NXY)
+        {
+            if (nI() == 0)
+                throw std::runtime_error("0 in I-dim.");
+            if (nJ() == 0)
+                throw std::runtime_error("0 in J-dim.");
+            if (nK() == 0)
+                throw std::runtime_error("0 in K-dim.");
+        }
+
+        virtual ~ArrayND() = default;
+
+        size_t nI() const
+        {
+            return m_Nx;
+        }
+
+        size_t nJ() const
+        {
+            return m_Ny;
+        }
+
+        size_t nK() const
+        {
+            return m_Nz;
+        }
+
+    private:
+        /// Calculate 0-based internal index
+        size_t idx(size_t i, size_t j) const
+        {
+            return i + m_Nx * j;
+        }
+
+        size_t idx(size_t i, size_t j, size_t k) const
+        {
+            return i + m_Nx * j + m_NXY * k;
+        }
+
+    public:
+        /// 2D
+        /// 0-based indexing
+        T at(size_t i, size_t j) const
+        {
+            const size_t n = idx(i, j);
+            return m_data[n];
+        }
+
+        T& at(size_t i, size_t j)
+        {
+            const size_t n = idx(i, j);
+            return m_data[n];
+        }
+
+        /// 1-based indexing
+        T operator()(size_t i, size_t j) const
+        {
+            return at(i - 1, j - 1);
+        }
+
+        T& operator()(size_t i, size_t j)
+        {
+            return at(i - 1, j - 1);
+        }
+
+        /// 3D
+        /// 0-based indexing
+        T at(size_t i, size_t j, size_t k) const
+        {
+            const size_t n = idx(i, j, k);
+            return m_data[n];
+        }
+
+        T& at(size_t i, size_t j, size_t k)
+        {
+            const size_t n = idx(i, j, k);
+            return m_data[n];
+        }
+
+        /// 1-based indexing
+        T operator()(size_t i, size_t j, size_t k) const
+        {
+            return at(i - 1, j - 1, k - 1);
+        }
+
+        T& operator()(size_t i, size_t j, size_t k)
+        {
+            return at(i - 1, j - 1, k - 1);
+        }
+    };
 }
 
 #endif
--- src/common.cc
+++ src/common.cc
@@ -1,127 +1,176 @@
 #include "../inc/common.h"
 
-namespace GridTool
+namespace GridTool::COMMON
 {
-	namespace COMMON
-	{
-		double relaxation(double a, double b, double x)
-		{
-			return (1.0 - x) * a + x * b;
-		}
+    Scalar relaxation(Scalar a, Scalar b, Scalar x)
+    {
+        return (1.0 - x) * a + x * b;
+    }
 
-		DIM::DIM(int dim, bool is3d) :
-			m_is3D(is3d)
-		{
-			if (dim == 2 || dim == 3)
-				m_dim = dim;
-			else
-				throw wrong_dimension(dim);
+    DIM::DIM(int dim, bool is3d) :
+        m_is3D(is3d)
+    {
+        if (dim == 2 || dim == 3)
+            m_dim = dim;
+        else
+            throw wrong_dimension(dim);
 
-			if (dim == 3 && !is3d)
-				throw std::invalid_argument("Inconsistent dimensions.");
-		}
+        if (dim == 3 && !is3d)
+            throw std::invalid_argument("Inconsistent dimensions.");
+    }
 
-		const Scalar &Vector::operator()(short idx) const
-		{
-			switch (idx)
-			{
-			case 1:
-				return x();
-			case 2:
-				return y();
-			case 3:
-				return z();
-			default:
-				throw not_vector_component(idx);
-			}
-		}
+    bool DIM::is3D() const
+    {
+        return m_is3D;
+    }
 
-		Scalar &Vector::operator()(short idx)
-		{
-			switch (idx)
-			{
-			case 1:
-				return x();
-			case 2:
-				return y();
-			case 3:
-				return z();
-			default:
-				throw not_vector_component(idx);
-			}
-		}
+    int DIM::dimension() const
+    {
+        return m_dim;
+    }
 
-		Vector &Vector::operator=(const Vector &rhs)
-		{
-			x() = rhs.x();
-			y() = rhs.y();
-			z() = rhs.z();
-			return *this;
-		}
+    const Scalar &Vector::operator()(short idx) const
+    {
+        switch (idx)
+        {
+        case 1:
+            return x();
+        case 2:
+            return y();
+        case 3:
+            return z();
+        case -1:
+            return z();
+        case -2:
+            return y();
+        case -3:
+            return x();
+        default:
+            throw not_vector_component(idx);
+        }
+    }
 
-		Vector &Vector::operator+=(const Vector &rhs)
-		{
-			x() += rhs.x();
-			y() += rhs.y();
-			z() += rhs.z();
-			return *this;
-		}
+    Scalar &Vector::operator()(short idx)
+    {
+        switch (idx)
+        {
+        case 1:
+            return x();
+        case 2:
+            return y();
+        case 3:
+            return z();
+        case -1:
+            return z();
+        case -2:
+            return y();
+        case -3:
+            return x();
+        default:
+            throw not_vector_component(idx);
+        }
+    }
 
-		Vector &Vector::operator-=(const Vector &rhs)
-		{
-			x() -= rhs.x();
-			y() -= rhs.y();
-			z() -= rhs.z();
-			return *this;
-		}
+    const Scalar &Vector::x() const
+    {
+        return at(0);
+    }
 
-		Vector &Vector::operator*=(Scalar a)
-		{
-			x() *= a;
-			y() *= a;
-			z() *= a;
-			return *this;
-		}
+    const Scalar &Vector::y() const
+    {
+        return at(1);
+    }
 
-		Vector &Vector::operator/=(Scalar a)
-		{
-			x() /= a;
-			y() /= a;
-			z() /= a;
-			return *this;
-		}
+    const Scalar &Vector::z() const
+    {
+        return at(2);
+    }
 
-		Scalar Vector::dot(const Vector &b) const
-		{
-			Scalar ret = 0.0;
-			ret += x() * b.x();
-			ret += y() * b.y();
-			ret += z() * b.z();
-			return ret;
-		}
+    Scalar &Vector::x()
+    {
+        return at(0);
+    }
 
-		Vector Vector::cross(const Vector &b) const
-		{
-			Vector ret;
-			ret.x() = y() * b.z() - z() * b.y();
-			ret.y() = z() * b.x() - x() * b.z();
-			ret.z() = x() * b.y() - y() * b.x();
-			return ret;
-		}
+    Scalar &Vector::y()
+    {
+        return at(1);
+    }
 
-		Scalar Vector::norm() const
-		{
-			Scalar ret = 0.0;
-			ret += std::pow(x(), 2);
-			ret += std::pow(y(), 2);
-			ret += std::pow(z(), 2);
-			return std::sqrt(ret);
-		}
+    Scalar &Vector::z()
+    {
+        return at(2);
+    }
 
-		void Vector::normalize()
-		{
-			const Scalar L = norm();
-			this->operator/=(L);
-		}
-	}
+    Vector &Vector::operator=(const Vector &rhs)
+    {
+        x() = rhs.x();
+        y() = rhs.y();
+        z() = rhs.z();
+        return *this;
+    }
+
+    Vector &Vector::operator+=(const Vector &rhs)
+    {
+        x() += rhs.x();
+        y() += rhs.y();
+        z() += rhs.z();
+        return *this;
+    }
+
+    Vector &Vector::operator-=(const Vector &rhs)
+    {
+        x() -= rhs.x();
+        y() -= rhs.y();
+        z() -= rhs.z();
+        return *this;
+    }
+
+    Vector &Vector::operator*=(Scalar a)
+    {
+        x() *= a;
+        y() *= a;
+        z() *= a;
+        return *this;
+    }
+
+    Vector &Vector::operator/=(Scalar a)
+    {
+        x() /= a;
+        y() /= a;
+        z() /= a;
+        return *this;
+    }
+
+    Scalar Vector::dot(const Vector &b) const
+    {
+        Scalar ret = 0.0;
+        ret += x() * b.x();
+        ret += y() * b.y();
+        ret += z() * b.z();
+        return ret;
+    }
+
+    Vector Vector::cross(const Vector &b) const
+    {
+        Vector ret;
+        ret.x() = y() * b.z() - z() * b.y();
+        ret.y() = z() * b.x() - x() * b.z();
+        ret.z() = x() * b.y() - y() * b.x();
+        return ret;
+    }
+
+    Scalar Vector::norm() const
+    {
+        Scalar ret = 0.0;
+        ret += std::pow(x(), 2);
+        ret += std::pow(y(), 2);
+        ret += std::pow(z(), 2);
+        return std::sqrt(ret);
+    }
+
+    void Vector::normalize()
+    {
+        const Scalar L = norm();
+        this->operator/=(L);
+    }
 }
